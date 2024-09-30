97a953545019ebcecbaa8012a070d5df2f0d738d
renerod1 <64755292+renerod1@users.noreply.github.com>
2024-09-30 04:23:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     tsconfigPaths(),
     svgr(),
   ],
-  base: '/',
+  base: '/midjourney-image-previewer/',
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
