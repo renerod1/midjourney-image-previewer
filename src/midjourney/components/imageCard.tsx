986914b8a21e401a2cd8f374a32a77d9003ad558
Rene Rodriguez <renerod1@hotmail.com>
2025-05-20 03:25:55 -0400

    fix: fixed lightbox

--- src/midjourney/components/imageCard.tsx
+++ src/midjourney/components/imageCard.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'preact/hooks'
-import 'bs5-lightbox'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import Lightbox from 'bs5-lightbox'
 
 interface IImageCard {
   batchNo: number
@@ -8,11 +8,12 @@ interface IImageCard {
 
 export const ImageCard = ({ batchNo, jobId }: IImageCard) => {
   const [isImage, setIsImage] = useState(false)
+  const lightboxRef = useRef<Lightbox | null>(null)
   const imageURL =
     'https://cdn.midjourney.com/' + jobId + '/0_' + (batchNo - 1) + '.png'
 
-  function imageExists(image_url) {
-    var img = new Image()
+  function imageExists(image_url: string) {
+    const img = new Image()
     img.src = image_url
     img.onload = function () {
       setIsImage(true)
@@ -21,21 +22,59 @@ export const ImageCard = ({ batchNo, jobId }: IImageCard) => {
 
   useEffect(() => {
     imageExists(imageURL)
-  }),
-    []
+  }, [imageURL])
+
+  useEffect(() => {
+    if (isImage) {
+      const link = document.querySelector(`a[href='${imageURL}']`)
+      if (link) {
+        const handleClick = (e: Event) => {
+          e.preventDefault()
+          if (!lightboxRef.current) {
+            lightboxRef.current = new Lightbox(link)
+          }
+          lightboxRef.current.show()
+        }
+
+        link.addEventListener('click', handleClick)
+
+        return () => {
+          link.removeEventListener('click', handleClick)
+          if (lightboxRef.current) {
+            lightboxRef.current.hide()
+          }
+        }
+      }
+    }
+  }, [isImage, imageURL])
 
   if (!isImage) {
     return <div class='col'></div>
   } else {
     return (
-      <div class='col'>
+      <div class='col col-sm-4'>
         <div class='card'>
           <div class='card-body'>
-            <a href={imageURL} data-toggle='lightbox' data-gallery='gallery'>
-              <img src={imageURL} class='img-thumbnail img-fluid' />
+            <a
+              href={imageURL}
+              class='col-sm-4'
+              data-bs-toggle='lightbox'
+              data-gallery='mj-gallery'
+              data-type='image'
+            >
+              <img
+                class='img-thumbnail img-fluid'
+                src={imageURL}
+                alt={`Image ${batchNo}`}
+              />
             </a>
             <h5 class='card-title'>Image {batchNo}:</h5>
-            <a class='card-text' href={imageURL} target='_blank'>
+            <a
+              class='card-text'
+              href={imageURL}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               {imageURL}
             </a>
           </div>
--- src/midjourney/components/imageGrid.tsx
+++ src/midjourney/components/imageGrid.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'preact/hooks'
-import 'bs5-lightbox'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import Lightbox from 'bs5-lightbox'
 
 interface IImageGrid {
   batchNo: number
@@ -7,11 +7,12 @@ interface IImageGrid {
 }
 export const ImageGrid = ({ batchNo, jobId }: IImageGrid) => {
   const [isImage, setIsImage] = useState(false)
+  const lightboxRef = useRef<Lightbox | null>(null)
   const imageURL =
     'https://cdn.midjourney.com/' + jobId + '/grid_' + (batchNo - 1) + '.png'
 
-  function imageExists(image_url) {
-    var img = new Image()
+  function imageExists(image_url: string) {
+    const img = new Image()
     img.src = image_url
     img.onload = function () {
       setIsImage(true)
@@ -20,8 +21,31 @@ export const ImageGrid = ({ batchNo, jobId }: IImageGrid) => {
 
   useEffect(() => {
     imageExists(imageURL)
-  }),
-    []
+  }, [imageURL])
+
+  useEffect(() => {
+    if (isImage) {
+      const link = document.querySelector(`a[href='${imageURL}']`)
+      if (link) {
+        const handleClick = (e: Event) => {
+          e.preventDefault()
+          if (!lightboxRef.current) {
+            lightboxRef.current = new Lightbox(link)
+          }
+          lightboxRef.current.show()
+        }
+
+        link.addEventListener('click', handleClick)
+
+        return () => {
+          link.removeEventListener('click', handleClick)
+          if (lightboxRef.current) {
+            lightboxRef.current.hide()
+          }
+        }
+      }
+    }
+  }, [isImage, imageURL])
 
   if (!isImage) {
     return <div class='col'></div>
@@ -30,11 +54,26 @@ export const ImageGrid = ({ batchNo, jobId }: IImageGrid) => {
       <div class='col-4'>
         <div class='card'>
           <div class='card-body'>
-            <a href={imageURL} data-toggle='lightbox' data-gallery='gallery'>
-              <img src={imageURL} class='img-thumbnail img-fluid' />
+            <a
+              href={imageURL}
+              class='col-sm-4'
+              data-bs-toggle='lightbox'
+              data-gallery='mj-gallery'
+              data-type='image'
+            >
+              <img
+                class='img-thumbnail img-fluid'
+                src={imageURL}
+                alt={`Image Grid ${batchNo}`}
+              />
             </a>
             <h5 class='card-title'>Image Grid {batchNo}:</h5>
-            <a class='card-text' href={imageURL} target='_blank'>
+            <a
+              class='card-text'
+              href={imageURL}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               {imageURL}
             </a>
           </div>
